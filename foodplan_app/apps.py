eb31d7f3f26021791f6f6707aa721686dbc6a9e3
inkvizitor1991 <85788805+inkvizitor1991@users.noreply.github.com>
2022-03-15 19:49:00 +0300

    Added the project

---
+++ foodplan_app/apps.py
@@ -0,0 +1,5 @@
+from django.apps import AppConfig
+
+
+class FoodplanAppConfig(AppConfig):
+    name = 'foodplan_app'
---
+++ foodplan_app/urls.py
@@ -0,0 +1,14 @@
+from django.urls import path
+from .views import (
+    base
+)
+
+
+
+
+
+
+
+urlpatterns = [
+    path('', base, name='base'),
+]
